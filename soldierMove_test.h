58bf0536732195ea2681f4321575e5a6cee121ed
dtalmood <dtalmood1@gmail.com>
2022-06-02 18:39:30 -0700

    Added Red object tests

--- soldierMove_test.h
+++ soldierMove_test.h
@@ -6,7 +6,7 @@
 #include "validMove.h"
 #include "MockClass.h"
 
-TEST(SolderValidMove, moveAhead) 
+TEST(SolderValidMoveBlack, blackMoveAhead) 
 {
     SoldierMock_ChessBoardSpot soldierObject;
     //               c  r  c  r
@@ -29,7 +29,7 @@ TEST(SolderValidMove, moveAhead)
     EXPECT_TRUE(soldierObject.move->move(occupied, BLACK, king));
 }
 
-TEST(SolderValidMove, passRiver) 
+TEST(SolderValidMoveBlack, blackPassRiver) 
 {
     SoldierMock_ChessBoardSpot soldierObject;
     //               c  r  c  r
@@ -52,7 +52,7 @@ TEST(SolderValidMove, passRiver)
     EXPECT_TRUE(soldierObject.move->move(occupied, BLACK, king));
 }
 
-TEST(SolderInvalidMove, MoveBackwards) 
+TEST(SolderInvalidMoveBlack, blackMoveBackwards) 
 {
     SoldierMock_ChessBoardSpot soldierObject;
     //               c  r  c  r
@@ -75,7 +75,7 @@ TEST(SolderInvalidMove, MoveBackwards)
     EXPECT_FALSE(soldierObject.move->move(occupied, BLACK, king));
 }
 
-TEST(SolderInvalidMove, MoveLeft) 
+TEST(SolderInvalidMoveBlack, blackMoveLeft) 
 {
     SoldierMock_ChessBoardSpot soldierObject;
     //               c  r  c  r
@@ -98,7 +98,7 @@ TEST(SolderInvalidMove, MoveLeft)
     EXPECT_FALSE(soldierObject.move->move(occupied, BLACK, king));
 }
 
-TEST(SolderInvalidMove, MoveRight) 
+TEST(SolderInvalidMoveBlack, blackMoveRight) 
 {
     SoldierMock_ChessBoardSpot soldierObject;
     //               c  r  c  r
@@ -119,6 +119,98 @@ TEST(SolderInvalidMove, MoveRight)
 
     vector<int> king = { 7, 1, 7, 9 };
     EXPECT_FALSE(soldierObject.move->move(occupied, BLACK, king));
+}
+
+TEST(SolderValidMoveRed, redMoveAhead) 
+{
+    SoldierMock_ChessBoardSpot soldierObject;
+    //                           c  r  c  r
+    soldierObject.move->setCoors(2, 8, 2, 7); 
+
+    bool occupied[10][9] =
+    {   {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+    };
+
+    vector<int> king = { 7, 1, 7, 9 };
+    EXPECT_TRUE(soldierObject.move->move(occupied, RED, king));
+}
+
+TEST(SolderInvalidMoveRed, redMoveBackwards) 
+{
+    SoldierMock_ChessBoardSpot soldierObject;
+    //                           c  r  c  r
+    soldierObject.move->setCoors(2, 8, 2, 9); 
+
+    bool occupied[10][9] =
+    {   {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+    };
+
+    vector<int> king = { 7, 1, 7, 9 };
+    EXPECT_FALSE(soldierObject.move->move(occupied, RED, king));
+}
+
+TEST(SolderInvalidMoveRed, redMoveLeft) 
+{
+    SoldierMock_ChessBoardSpot soldierObject;
+    //                           c  r  c  r
+    soldierObject.move->setCoors(2, 8, 1, 8); 
+
+    bool occupied[10][9] =
+    {   {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+    };
+
+    vector<int> king = { 7, 1, 7, 9 };
+    EXPECT_FALSE(soldierObject.move->move(occupied, RED, king));
+}
+
+TEST(SolderInvalidMoveRed, redMoveRight) 
+{
+    SoldierMock_ChessBoardSpot soldierObject;
+    //                           c  r  c  r
+    soldierObject.move->setCoors(2, 8, 3, 8); 
+
+    bool occupied[10][9] =
+    {   {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0},
+    };
+
+    vector<int> king = { 7, 1, 7, 9 };
+    EXPECT_FALSE(soldierObject.move->move(occupied, RED, king));
 }
 
 #endif
